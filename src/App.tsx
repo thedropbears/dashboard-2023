449605539959361a4d27a341f89d435a8ef2c6fc
Pol Selles Climent <81540683+Pokesi@users.noreply.github.com>
2023-01-22 12:52:41 +1100

    Add NT functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import React, { useState } from "react";
 import "./App.css";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { NetworkTables, NetworkTableTypeInfos } from 'ntcore-ts-client';
 
-type cubeOrCone = "cube" | "cone" | "none";
+type cubeOrCone = "Cube" | "Cone" | "None";
+
+function useNTInstance(portOrURI: number | string) {
+  return (typeof portOrURI === "number") ? NetworkTables.createInstanceByTeam(4774, portOrURI) : NetworkTables.createInstanceByURI(portOrURI);
+}
 
 function ConeOrCube(...props: any): JSX.Element {
   const piece: cubeOrCone = props[0].piece;
-
-  // [{ piece: value }]
 
   return (
     <div
       className="coneOrCube"
       style={{
-        // backgroundColor: piece === "cube" ? "violet" : "yellow",
         backgroundColor:
-          piece === "none" ? "grey" : piece === "cube" ? "violet" : "yellow",
+          piece === "None" ? "grey" : piece === "Cube" ? "violet" : "yellow",
       }}
     >
       <p className="text">{piece}</p>
@@ -24,12 +26,9 @@ function ConeOrCube(...props: any): JSX.Element {
   );
 }
 
-function Table() {
-  const [table, setTable] = useState<boolean[][]>([
-    [false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false],
-    [false, false, false, false, false, false, false, false, false],
-  ]);
+function Table(...props: any) {
+  const table: boolean[][] = props[0].table;
+  const setTable: any = props[0].setTable;
 
   function handleButtonClick(column: number, row: number) {
     const tableClone = [...table];
@@ -39,64 +38,93 @@ function Table() {
 
   return (
     <table className="table">
-      <tr>
-        {table[0].map(function (item, key) {
-          return (
-            <td>
-              <Button
-                onClick={() => {
-                  handleButtonClick(key, 0);
-                }}
-                variant="contained"
-              >
-                {item ? "o" : "x"}
-              </Button>
-            </td>
-          );
-        })}
-      </tr>
-      <tr>
-        {table[1].map(function (item, key) {
-          return (
-            <td>
-              <Button
-                onClick={() => {
-                  handleButtonClick(key, 1);
-                }}
-                variant="contained"
-              >
-                {item ? "o" : "x"}
-              </Button>
-            </td>
-          );
-        })}
-      </tr>
-      <tr>
-        {table[2].map(function (item, key) {
-          return (
-            <td>
-              <Button
-                onClick={() => {
-                  handleButtonClick(key, 2);
-                }}
-                variant="contained"
-              >
-                {item ? "o" : "x"}
-              </Button>
-            </td>
-          );
-        })}
-      </tr>
+      {[0, 1, 2].map(function (unused, row) {
+        return (
+          <tr>
+            {table[row].map(function (item, key) {
+              return (
+                <td>
+                  <p style={{ fontSize: '12px' }}>Node ID #{row * 9 + key}</p>
+                  <Button
+                    onClick={() => {
+                      handleButtonClick(key, row);
+                    }}
+                    variant="contained"
+                    className="gridButton"
+                    style={{
+                      backgroundColor: item ? "green" : "red"  
+                    }}
+                  >
+                    {item ? "o" : "x"}
+                  </Button>
+                </td>
+              );
+            })}
+          </tr>
+        );
+      })}
     </table>
   );
 }
 
+function splitArray(array: any[], times: number) {
+
+  const result = array.reduce((resultArray, item, index) => { 
+    const chunkIndex = Math.floor(index/times)
+
+    if(!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = [] // start a new chunk
+    }
+
+    resultArray[chunkIndex].push(item)
+
+    return resultArray
+  }, [])
+
+  return result;
+}
+
 function App() {
-  const [piece, setPiece] = useState<cubeOrCone>("none");
+  const [piece, setPiece] = useState<cubeOrCone>("None");
+  const [table, setTable] = useState<boolean[][]>([
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false, false],
+  ]);
+
+  const instance = useNTInstance(5810);
+
+  /*
+    0: none
+    1: cone
+    2: cube
+   */
+  const pickupTopic = instance.createTopic<number>('PickupObject', NetworkTableTypeInfos.kInteger, 0);
+  const  nodesTopic = instance.createTopic<boolean[]>('NodeObject', NetworkTableTypeInfos.kBooleanArray, Array(27).fill(false));
 
   function handleCheck(e: any) {
     setPiece(e.target.value);
   }
+
+  pickupTopic.subscribe((value: number | null) => {
+    const toType: cubeOrCone = ({
+      0: "None",
+      1: "Cone",
+      2: "Cube"
+    } as { [key: number]: cubeOrCone })[value || 0]
+
+    // Don't update the DOM if the value hasn't changed
+    if (toType !== piece) {
+      setPiece(toType);
+    }
+  }, false);
+
+  nodesTopic.subscribe((value: boolean[] | null) => {
+    const splitted = splitArray((value || []), 9);
+    if (splitted !== table) { 
+      setPiece(splitted);
+    }
+  }, false);
 
   return (
     <div className="App">
@@ -105,42 +133,44 @@ function App() {
         <div>
           <ConeOrCube piece={piece} />
           <form onSubmit={handleCheck}>
-            <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="cone"
-                  checked={piece === "cone"}
-                  onChange={handleCheck}
-                />
-                Cone
-              </label>
-            </div>
-            <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="cube"
-                  checked={piece === "cube"}
-                  onChange={handleCheck}
-                />
-                Cube
-              </label>
-            </div>
-            <div className="radio">
-              <label>
-                <input
-                  type="radio"
-                  value="none"
-                  checked={piece === "none"}
-                  onChange={handleCheck}
-                />
-                None
-              </label>
-            </div>
+            <Stack direction="row">
+              <div className="radio">
+                <label>
+                  <input
+                    type="radio"
+                    value="Cone"
+                    checked={piece === "Cone"}
+                    onChange={handleCheck}
+                  />
+                  Cone
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input
+                    type="radio"
+                    value="Cube"
+                    checked={piece === "Cube"}
+                    onChange={handleCheck}
+                  />
+                  Cube
+                </label>
+              </div>
+              <div className="radio">
+                <label>
+                  <input
+                    type="radio"
+                    value="None"
+                    checked={piece === "None"}
+                    onChange={handleCheck}
+                  />
+                  None
+                </label>
+              </div>
+            </Stack>
           </form>
         </div>
-        <Table />
+        <Table table={table} setTable={setTable}/>
       </Stack>
     </div>
   );
