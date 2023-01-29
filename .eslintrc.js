7284b3df968ee22d2f7e9acc13702d4852802bb2
David Vo <auscompgeek@users.noreply.github.com>
2023-01-29 12:11:57 +1100

    Enable recommended ESLint rules and run in CI

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "react-app",
+    "react-app/jest",
+    "prettier",
+  ],
+  root: true,
+};
